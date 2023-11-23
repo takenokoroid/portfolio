3ae8f1e32a1d187804ac697458180c615880a201
takenokoroid <takenokoroid@gmail.com>
2023-11-23 07:58:03 +0000

    update: それぞれのページ例を簡単に作成

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<header>
+	<h1>
+		<a href="/">たけろいど</a>
+	</h1>
+	<ul>
+		<li><a href="/about">ABOUT</a></li>
+		<li><a href="/contact">CONTACT</a></li>
+	</ul>
+</header>
+<main>
+	<slot />
+</main>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+	}
+	ul {
+		display: flex;
+		list-style: none;
+	}
+	li {
+		margin-left: 1rem;
+	}
+</style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,6 @@
+<h2>ABOUT</h2>
+
+<ul>
+	<li>Github</li>
+	<li>X</li>
+</ul>
